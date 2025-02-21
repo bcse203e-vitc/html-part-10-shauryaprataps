55d0e9cc23e99e54411989a8a3e49d1035ec0155
shauryaprataps <shauryaprataps1303@gmail.com>
2025-02-21 13:37:33 +0530

    Create q5.js

---
+++ Assignment-10/q5.js
@@ -0,0 +1,9 @@
+document.getElementById("darkModeBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+});
+
+document.getElementById("lightModeBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+});
