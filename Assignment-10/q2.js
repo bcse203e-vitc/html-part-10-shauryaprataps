f5426dffa3dd8960828618ed5ba9b498eb896ef6
shauryaprataps <shauryaprataps1303@gmail.com>
2025-02-21 13:22:34 +0530

    Create q2.js

---
+++ Assignment-10/q2.js
@@ -0,0 +1,13 @@
+
+const redButton = document.getElementById("redBtn");
+const blueButton = document.getElementById("blueBtn");
+
+
+redButton.addEventListener("click", function() {
+    document.body.style.backgroundColor = "red";
+});
+
+
+blueButton.addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue";
+});
